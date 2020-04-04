46c451bda8f72839da89af17aa486c3449e935c5
riibeirogabriel <gabriel.oliveira432@live.com>
2020-04-04 08:41:42 -0300

    fix format code

--- process_service/server.py
+++ process_service/server.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello Worggggld"
-
+    return "Hello World"
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
