cbdc2dcff92bc23ab259cafffe66963d80cee01d
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-04-29 15:35:55 -0300

    Adding replica set

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 echo "learningOrschestra: a machine learning resource orchestrator"
 echo "------------------------------------------------"
-echo "Buiding own service images..."
+echo "Buiding own images service..."
 echo "------------------------------------------------"
 
 docker-compose build 
@@ -28,13 +28,34 @@ echo "------------------------------------------------"
 
 docker stack deploy --compose-file=docker-compose.yml service
 
-
 echo "------------------------------------------------"
 echo "Pushing the own service images in local repository..."
 echo "------------------------------------------------"
 
 sleep 20
 docker push 127.0.0.1:5050/database_api:database_api
+
+echo "------------------------------------------------"
+echo "Configuring replica set in database services..."
+echo "------------------------------------------------"
+
+sleep 20
+
+for replica_set in database database_replica_1 database_replica_2;
+    do  mongo --host $replica_set --eval 'db'
+    if [ $? -ne 0 ]; 
+        then exit 1
+    fi
+done
+
+status=$(mongo --host database --quiet --eval 'rs.status().members.length')
+
+if [ $? -ne 0 ]; 
+    then mongo --host database --eval 'rs.initiate({ _id: "replica_set", version: 1, '\
+                                      'members: [ { _id: 0, host : "database" }, '\
+                                      '{ _id: 1, host : "database_replica_2" },'\
+                                      ' { _id: 2, host : "database_replica_1" } ] })';
+fi
 
 echo "------------------------------------------------"
 echo "End."
