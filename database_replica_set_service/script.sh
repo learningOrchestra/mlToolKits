0bba4fd654d8167490ffd8fae2e968f127963cd5
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-04-29 18:07:55 -0300

    Bug fixes

--- database_replica_set_service/script.sh
+++ database_replica_set_service/script.sh
@@ -10,5 +10,5 @@ done
 status=$(mongo --host database_1 --quiet --eval 'rs.status().members.length')
 
 if [ $? -ne 0 ];
-    then mongo --host database_1 --eval 'rs.initiate({ _id: "replica_set", version: 1, members: [ { _id: 0, host : "database_1" }, { _id: 1, host : "database_2" }, { _id: 2, host : "database_3" } ] })';
+    then mongo --host database_1 --eval 'rs.initiate({ _id: "replica_set", version: 1, members: [ { _id: 0, host : "database_1:27017" }, { _id: 1, host : "database_2:27017" }, { _id: 2, host : "database_3:27017" } ] })';
 fi
