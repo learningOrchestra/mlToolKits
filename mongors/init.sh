f3357f1e91558641d938dee393fd834f1ed3bfb8
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-04-30 11:23:23 -0300

    Add port from database services

--- mongors/init.sh
+++ mongors/init.sh
@@ -9,5 +9,5 @@ done
 
 status=$(mongo --host rs1 --quiet --eval 'rs.status().members.length')
 if [ $? -ne 0 ]; then
-  mongo --host rs1 --eval 'rs.initiate({ _id: "rs0", version: 1, members: [ { _id: 0, host : "rs1" }, { _id: 1, host : "rs2" }, { _id: 2, host : "rs3" } ] })';
+  mongo --host rs1 --eval 'rs.initiate({ _id: "rs0", version: 1, members: [ { _id: 0, host : "rs1:27017" }, { _id: 1, host : "rs2:27018" }, { _id: 2, host : "rs3:27019" } ] })';
 fi
