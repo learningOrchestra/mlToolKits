9cfabca7ecbda9536b2d0646b1500ccefefe8ef9
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-05-01 07:22:41 -0300

    updating structure services

--- mongors/init.sh
+++ mongors/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+# Make sure 3 replicas available
 
 for rs in rs1 rs2 rs3;do
   mongo --host $rs --eval 'db'
@@ -7,7 +7,11 @@ for rs in rs1 rs2 rs3;do
   fi
 done
 
+# Connect to rs1 and configure replica set if not done
+
 status=$(mongo --host rs1 --quiet --eval 'rs.status().members.length')
+
 if [ $? -ne 0 ]; then
-  mongo --host rs1 --eval 'rs.initiate({ _id: "rs0", version: 1, members: [ { _id: 0, host : "rs1:27017" }, { _id: 1, host : "rs2:27018" }, { _id: 2, host : "rs3:27019" } ] })';
+  # Replicaset not yet configured
+  mongo --host rs1 --eval 'rs.initiate({ _id: "rs0", version: 1, members: [ { _id: 0, host : "rs1" }, { _id: 1, host : "rs2" }, { _id: 2, host : "rs3" } ] })';
 fi
