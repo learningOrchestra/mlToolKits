419d6fea59c3c6f73f597ab01d2fa7457eaae510
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-05-15 11:10:07 -0300

    Bug fix

--- database_api_service/database_api.py
+++ database_api_service/database_api.py
@@ -11,89 +11,99 @@ from gridfs import GridFSBucket
 import requests
 import json
 
-http_status_code_success = 200
-http_status_code_sucess_created = 201
-http_status_code_not_found = 404
-http_status_code_server_error = 500
-file_handler_name = "response_handler"
+HTTP_STATUS_CODE_SUCESS = 200
+HTTP_STATUS_CODE_SUCESS_CREATED = 201
+HTTP_STATUS_CODE_NOT_FOUND = 404
+HTTP_STATUS_CODE_SERVER_ERROR = 500
+FILE_HANDLER_NAME = "response_handler"
+MESSAGE_INVALID_URL = "invalid_url"
+MESSAGE_RESULT = "result"
+MESSAGE_CREATED_FILE = "file_created"
+MESSAGE_FILENAME = "filename"
+MESSAGE_DELETED_FILE = "deleted_file"
+MESSAGE_INVALID_PARAMETER = "invalid_parameter"
+CHUNK_SIZE = 1024
+
+DATABASE_API_HOST = "DATABASE_API_HOST"
+DATABASE_API_PORT = "DATABASE_API_PORT"
+DATABASE_URL = "DATABASE_URL"
+DATABASE_PORT = "DATABASE_PORT"
 
 app = Flask(__name__)
 
-mongo = MongoClient(os.environ["DATABASE_URL"],
-                    int(os.environ["DATABASE_PORT"]))
+mongo = MongoClient(os.environ[DATABASE_URL],
+                    int(os.environ[DATABASE_PORT]))
 files_gridfs = gridfs.GridFS(mongo.db)
 files_bucket = GridFSBucket(mongo.db)
 
 
 @app.route('/add', methods=['POST'])
 def add_file():
-    global http_status_code_sucess_created, http_status_code_success
-
     try:
         response = requests.get(request.json["url"], stream=True)
-        if(response.status_code != http_status_code_success):
-            return jsonify("invalid_url"), response.status_code
+
+        if(response.status_code != HTTP_STATUS_CODE_SUCESS):
+            return jsonify({MESSAGE_RESULT: MESSAGE_INVALID_URL}), \
+                response.status_code
     except Exception:
-        return jsonify("invalid_url"), http_status_code_server_error
+        return jsonify({MESSAGE_RESULT: MESSAGE_INVALID_URL}), \
+            HTTP_STATUS_CODE_SERVER_ERROR
 
-    response_handler = open(file_handler_name, "wb")
+    response_handler = open(FILE_HANDLER_NAME, "wb")
 
-    for chunk in response.iter_content(chunk_size=1024):
+    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
         if chunk:
             response_handler.write(chunk)
 
     response_handler.close()
-    response_file = open(file_handler_name, "rb")
+    response_file = open(FILE_HANDLER_NAME, "rb")
 
-    file_stream = files_bucket.open_upload_stream(request.json["filename"],
-                                                  chunk_size_bytes=1024,
-                                                  metadata=request.json)
+    file_stream = files_bucket.open_upload_stream(
+                        request.json[MESSAGE_FILENAME],
+                        chunk_size_bytes=CHUNK_SIZE,
+                        metadata=request.json)
 
     file_stream.write(response_file)
     file_stream.close()
     response_file.close()
 
-    return jsonify("file_created"), http_status_code_sucess_created
+    return jsonify({MESSAGE_RESULT: MESSAGE_CREATED_FILE}), \
+        HTTP_STATUS_CODE_SUCESS_CREATED
 
 
 @app.route('/files')
 def files():
-    global http_status_code_success
-
     result = []
-    for files in files_gridfs.find():
-        result.append(files.read())
+    for file in files_gridfs.find():
+        result.append(json.loads(file.read()))
 
-    return {'response': result}, http_status_code_success
+    return jsonify({MESSAGE_RESULT: result}), \
+        HTTP_STATUS_CODE_SUCESS
 
 
 @app.route('/file/<filename>',)
 def file(filename):
-    global http_status_code_success
-
     result = []
-    for files in files_gridfs.find({"filename": filename}):
-        result.append(files.read())
+    for file in files_gridfs.find({MESSAGE_FILENAME: filename}):
+        result.append(json.loads(file.read()))
 
-    return {'response': result}, http_status_code_success
+    return jsonify({MESSAGE_RESULT: result}), \
+        HTTP_STATUS_CODE_SUCESS
 
 
 @app.route('/delete/<filename>', methods=['DELETE'])
 def delete_file(filename):
-    global http_status_code_success
+    try:
+        file_id = files_gridfs.get_version(filename)._id
+        files_gridfs.delete(file_id)
+    except Exception:
+        return jsonify({MESSAGE_RESULT: MESSAGE_INVALID_PARAMETER}), \
+            HTTP_STATUS_CODE_SERVER_ERROR
 
-    file_id = files_gridfs.get_version(filename)._id
-    files_gridfs.delete(file_id)
-
-    return jsonify("file_deleted"), http_status_code_success
-
-
-@app.errorhandler(http_status_code_not_found)
-def not_found(error=None):
-    global http_status_code_not_found
-    return jsonify('not_found: ' + request.url), http_status_code_not_found
+    return jsonify({MESSAGE_RESULT: MESSAGE_DELETED_FILE}), \
+        HTTP_STATUS_CODE_SUCESS
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ["DATABASE_API_HOST"],
-            port=int(os.environ["DATABASE_API_PORT"]))
+    app.run(host=os.environ[DATABASE_API_HOST],
+            port=int(os.environ[DATABASE_API_PORT]))
