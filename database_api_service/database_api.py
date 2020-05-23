ae083a8601e84c0f08b724f143eebf3456678152
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-05-23 15:06:02 -0300

    parallelizing download file stages with threads

--- database_api_service/database_api.py
+++ database_api_service/database_api.py
@@ -6,7 +6,9 @@ from contextlib import closing
 import csv
 import json
 import codecs
-
+import threading
+from concurrent.futures import ThreadPoolExecutor, wait
+import time
 
 DATABASE_URL = "DATABASE_URL"
 DATABASE_PORT = "DATABASE_PORT"
@@ -25,25 +27,9 @@ class DatabaseApi:
         self.database = self.mongo_client.database
 
     def add_file(self, url, filename):
-        result = []
         try:
-            with closing(requests.get(url, stream=True)) as r:
-                reader = csv.reader(
-                    codecs.iterdecode(r.iter_lines(), encoding='utf-8'),
-                    delimiter=',', quotechar='"')
-
-                count = 1
-                file_collection = self.database[filename]
-                headers = next(reader)
-
-                for row in reader:
-                    json_object = {headers[indice]: row[indice]
-                                   for indice
-                                   in range(len(headers))}
-
-                    json_object["_id"] = count
-                    file_collection.insert_one(json_object)
-                    count += 1
+            file_storage = FileStorage(filename, url, self.database[filename])
+            file_storage.start()
 
         except requests.exceptions.RequestException:
             return DatabaseApi.MESSAGE_INVALID_URL
@@ -74,3 +60,55 @@ class DatabaseApi:
             result.append(item)
 
         return result
+
+
+class FileStorage:
+    DELAY_BETWEEN_STAGES = 1
+
+    def __init__(self, filename, url, database_connection):
+        self.thread_pool = ThreadPoolExecutor()
+        self.filename = filename
+        self.url = url
+        self.database_connection = database_connection
+        self.download_tratament_queue = []
+        self.tratament_save_queue = []
+        self.file_headers = None
+
+    def download_file(self):
+        with closing(requests.get(self.url, stream=True)) as r:
+            reader = csv.reader(
+                codecs.iterdecode(r.iter_lines(), encoding='utf-8'),
+                delimiter=',', quotechar='"')
+            self.file_headers = next(reader)
+            count = 1
+
+            for row in reader:
+                self.download_tratament_queue.insert(0, row)
+
+    def tratament_file(self):
+        count = 1
+
+        while(self.download_tratament_queue):
+            downloaded_row = self.download_tratament_queue.pop()
+
+            json_object = {self.file_headers[index]: downloaded_row[index]
+                           for index
+                           in range(len(self.file_headers))}
+
+            json_object["_id"] = count
+            self.tratament_save_queue.insert(0, json_object)
+            count += 1
+
+    def save_file(self):
+        while(self.tratament_save_queue):
+            self.database_connection.insert_one(
+                self.tratament_save_queue.pop())
+
+    def start(self):
+        self.thread_pool.submit(self.download_file())
+        time.sleep(self.DELAY_BETWEEN_STAGES)
+
+        self.thread_pool.submit(self.tratament_file())
+        time.sleep(self.DELAY_BETWEEN_STAGES)
+
+        self.thread_pool.submit(self.save_file())
