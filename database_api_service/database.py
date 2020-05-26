e4b940db460df2ad683fc7d77d32458091114f4c
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-05-26 09:33:47 -0300

    Refactoring system, to reduce module linkage

---
+++ database_api_service/database.py
@@ -0,0 +1,214 @@
+import os
+from pymongo import MongoClient, errors
+from bson.json_util import dumps
+import requests
+from contextlib import closing
+import csv
+import json
+import codecs
+import threading
+from concurrent.futures import ThreadPoolExecutor, wait
+import time
+from queue import Queue
+
+ROW_ID = "_id"
+METADATA_ROW_ID = 0
+
+
+class DatabaseApi:
+    MESSAGE_INVALID_URL = "invalid_url"
+    MESSAGE_DUPLICATE_FILE = "duplicate_file"
+    MESSAGE_CREATED_FILE = "file_created"
+    MESSAGE_DELETED_FILE = "deleted_file"
+
+    def __init__(self, database_object, file_manager_object):
+        self.database_object = database_object
+        self.file_manager_object = file_manager_object
+
+    def add_file(self, url, filename):
+        try:
+            file_storage = self.file_manager_object.storage_file(
+                filename, url, self.database_object)
+
+        except requests.exceptions.RequestException:
+            return DatabaseApi.MESSAGE_INVALID_URL
+
+        except errors.PyMongoError:
+            return DatabaseApi.MESSAGE_DUPLICATE_FILE
+
+        return DatabaseApi.MESSAGE_CREATED_FILE
+
+    def read_file(self, filename, skip, limit, query):
+        result = []
+
+        query_object = json.loads(query)
+        skip = int(skip)
+        limit = int(limit)
+
+        for file in self.database_object.find_in_file(
+                                                      filename, query_object,
+                                                      skip, limit):
+            result.append(json.loads(dumps(file)))
+
+        return result
+
+    def delete_file(self, filename):
+        self.database_object.delete_file(filename)
+        return DatabaseApi.MESSAGE_DELETED_FILE
+
+    def get_files(self):
+        result = []
+        skip_result = 0
+        limit_result = 0
+
+        for file in self.database_object.get_filenames():
+
+            metadata_file = self.database_object.find_one_in_file(
+                file, {ROW_ID: METADATA_ROW_ID})
+
+            metadata_file.pop(ROW_ID)
+            result.append(metadata_file)
+
+        return result
+
+
+class DatabaseInterface():
+    def connection(self, filename):
+        pass
+
+    def find_in_file(self, filename, query, skip, limit):
+        pass
+
+    def delete_file(self, filename):
+        pass
+
+    def find_one_in_file(self, filename, query):
+        pass
+
+    def get_filenames(self):
+        pass
+
+    def insert_one_in_file(self, filename, json_object):
+        pass
+
+    def update_one_in_file(self, filename, new_value, query):
+        pass
+
+
+class FileManagerInterface():
+    def storage_file(self, filename, url, database_connection):
+        pass
+
+
+class MongoOperations(DatabaseInterface):
+    DATABASE_URL = "DATABASE_URL"
+    DATABASE_PORT = "DATABASE_PORT"
+
+    def __init__(self):
+        self.mongo_client = MongoClient(
+            os.environ[self.DATABASE_URL],
+            int(os.environ[self.DATABASE_PORT]))
+        self.database = self.mongo_client.database
+
+    def connection(self, filename):
+        return self.database[filename]
+
+    def find_in_file(self, filename, query, skip=0, limit=1):
+        file_collection = self.database[filename]
+        return file_collection.find(query).skip(skip).limit(limit)
+
+    def delete_file(self, filename):
+        file_collection = self.database[filename]
+        file_collection.drop()
+
+    def get_filenames(self):
+        return self.database.list_collection_names()
+
+    def insert_one_in_file(self, filename, json_object):
+        file_collection = self.database[filename]
+        file_collection.insert_one(json_object)
+
+    def update_one_in_file(self, filename, new_value, query):
+        file_collection = self.database[filename]
+        file_collection.update_one(new_value, query)
+
+    def find_one_in_file(self, filename, query):
+        file_collection = self.database[filename]
+        return file_collection.find_one(query)
+
+
+class FileDownloaderAndSaver(FileManagerInterface):
+    MAX_QUEUE_SIZE = 1000
+    MAX_NUMBER_THREADS = 3
+    FINISHED = "finished"
+    file_headers = None
+
+    def __init__(self):
+        self.thread_pool = ThreadPoolExecutor(
+            max_workers=self.MAX_NUMBER_THREADS)
+        self.download_tratament_queue = Queue(maxsize=self.MAX_QUEUE_SIZE)
+        self.tratament_save_queue = Queue(maxsize=self.MAX_QUEUE_SIZE)
+
+    def download_file(self, url):
+        with closing(requests.get(url, stream=True)) as r:
+            reader = csv.reader(
+                codecs.iterdecode(r.iter_lines(), encoding='utf-8'),
+                delimiter=',', quotechar='"')
+            self.file_headers = next(reader)
+
+            for row in reader:
+                self.download_tratament_queue.put(row)
+
+        self.download_tratament_queue.put(self.FINISHED)
+
+    def tratament_file(self):
+        row_count = 1
+
+        while(True):
+            downloaded_row = self.download_tratament_queue.get()
+
+            if(downloaded_row == self.FINISHED):
+                break
+
+            json_object = {self.file_headers[index]: downloaded_row[index]
+                           for index
+                           in range(len(self.file_headers))}
+
+            json_object[ROW_ID] = row_count
+
+            self.tratament_save_queue.put(json_object)
+            row_count += 1
+
+        self.tratament_save_queue.put(self.FINISHED)
+
+    def save_file(self, database_connection, filename):
+        while(True):
+            json_object = self.tratament_save_queue.get()
+
+            if(json_object == self.FINISHED):
+                break
+
+            database_connection.insert_one_in_file(filename, json_object)
+
+        database_connection.update_one_in_file(
+            filename,
+            {ROW_ID: METADATA_ROW_ID}, {
+                '$set': {
+                    self.FINISHED: True
+                }})
+
+    def storage_file(self, filename, url, database_connection):
+        metadata_file = {
+            'filename': filename,
+            'url': url,
+            ROW_ID: METADATA_ROW_ID,
+            self.FINISHED: False
+        }
+        database_connection.insert_one_in_file(filename, metadata_file)
+
+        self.thread_pool.submit(self.download_file, url)
+
+        self.thread_pool.submit(self.tratament_file)
+
+        self.thread_pool.submit(
+            self.save_file, database_connection, filename)
--- database_api_service/server.py
+++ database_api_service/server.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request, Flask
 import os
-from database_api import DatabaseApi
+from database import FileDownloaderAndSaver, DatabaseApi, MongoOperations
 from flask_cors import CORS
 
 HTTP_STATUS_CODE_SUCESS = 200
@@ -21,56 +21,66 @@ DELETE = 'DELETE'
 app = Flask(__name__)
 CORS(app)
 
-database = DatabaseApi()
+
+@app.route('/files', methods=[POST])
+def create_file():
+    file_downloader_and_saver = FileDownloaderAndSaver()
+    mongo_operations = MongoOperations()
+    database = DatabaseApi(mongo_operations, file_downloader_and_saver)
+
+    result = database.add_file(
+        request.json["url"],
+        request.json[FILENAME])
+
+    if(result == DatabaseApi.MESSAGE_INVALID_URL):
+        return jsonify(
+            {MESSAGE_RESULT: DatabaseApi.MESSAGE_INVALID_URL}),\
+                HTTP_STATUS_CODE_SERVER_ERROR
+
+    elif(result == DatabaseApi.MESSAGE_DUPLICATE_FILE):
+        return jsonify(
+            {MESSAGE_RESULT: DatabaseApi.MESSAGE_DUPLICATE_FILE}),\
+                HTTP_STATUS_CODE_SERVER_ERROR
+
+    else:
+        return jsonify(
+            {MESSAGE_RESULT: DatabaseApi.MESSAGE_CREATED_FILE}),\
+                HTTP_STATUS_CODE_SUCESS_CREATED
 
 
-@app.route('/files', methods=[GET, DELETE, POST])
-def file_manager():
-    if(request.method == POST):
+@app.route('/files', methods=[GET])
+def read_files():
+    file_downloader_and_saver = FileDownloaderAndSaver()
+    mongo_operations = MongoOperations()
+    database = DatabaseApi(mongo_operations, file_downloader_and_saver)
 
-        result = database.add_file(
-            request.json["url"],
-            request.json[FILENAME])
+    if(request.args):
+        file_result = database.read_file(
+            request.args.get(FILENAME), request.args.get('skip'),
+            request.args.get('limit'), request.args.get('query'))
 
-        if(result == DatabaseApi.MESSAGE_INVALID_URL):
-            return jsonify(
-                {MESSAGE_RESULT: DatabaseApi.MESSAGE_INVALID_URL}),\
-                    HTTP_STATUS_CODE_SERVER_ERROR
+        return jsonify(
+            {MESSAGE_RESULT: file_result}), HTTP_STATUS_CODE_SUCESS
 
-        elif(result == DatabaseApi.MESSAGE_DUPLICATE_FILE):
-            return jsonify(
-                {MESSAGE_RESULT: DatabaseApi.MESSAGE_DUPLICATE_FILE}),\
-                    HTTP_STATUS_CODE_SERVER_ERROR
+    else:
+        return jsonify({MESSAGE_RESULT: database.get_files()}),\
+            HTTP_STATUS_CODE_SUCESS
 
-        else:
-            return jsonify(
-                {MESSAGE_RESULT: DatabaseApi.MESSAGE_CREATED_FILE}),\
-                    HTTP_STATUS_CODE_SUCESS_CREATED
 
-    elif(request.method == GET):
+@app.route('/files', methods=[DELETE])
+def delete_file():
+    file_downloader_and_saver = FileDownloaderAndSaver()
+    mongo_operations = MongoOperations()
+    database = DatabaseApi(mongo_operations, file_downloader_and_saver)
 
-        if(request.args):
-            file_result = database.read_file(
-                request.args.get(FILENAME), request.args.get('skip'),
-                request.args.get('limit'), request.args.get('query'))
+    result = database.delete_file(request.json[FILENAME])
 
-            return jsonify(
-                {MESSAGE_RESULT: file_result}), HTTP_STATUS_CODE_SUCESS
-
-        else:
-            return jsonify({MESSAGE_RESULT: database.get_files()}),\
+    if(result == DatabaseApi.MESSAGE_DELETED_FILE):
+        return jsonify(
+            {MESSAGE_RESULT: DatabaseApi.MESSAGE_DELETED_FILE}),\
                 HTTP_STATUS_CODE_SUCESS
-
-    elif(request.method == DELETE):
-
-        result = database.delete_file(request.json[FILENAME])
-
-        if(result == DatabaseApi.MESSAGE_DELETED_FILE):
-            return jsonify(
-                {MESSAGE_RESULT: DatabaseApi.MESSAGE_DELETED_FILE}),\
-                    HTTP_STATUS_CODE_SUCESS
 
 
 if __name__ == "__main__":
     app.run(host=os.environ[DATABASE_API_HOST],
-            port=int(os.environ[DATABASE_API_PORT]))
+            port=int(os.environ[DATABASE_API_PORT]), debug=True)
