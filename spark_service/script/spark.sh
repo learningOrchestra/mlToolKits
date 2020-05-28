a14cdb174e7044408d5ee7157ea87d78d2c429ea
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-05-28 12:43:11 -0300

    Creating own spark service

---
+++ spark_service/script/spark.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$SPARK_ROLE" = "master" ]; then
+  $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
+fi
+if [ "$SPARK_ROLE" = "slave" ]; then
+  $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER:$SPARK_MASTER_PORT
+fi
