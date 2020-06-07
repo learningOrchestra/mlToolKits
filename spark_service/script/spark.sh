24f0a4077cbe89a81e8d95237e001751d9e8c982
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-06-07 19:05:25 -0300

    Add port to sparkworker

--- spark_service/script/spark.sh
+++ spark_service/script/spark.sh
@@ -4,5 +4,5 @@ if [ "$SPARK_ROLE" = "master" ]; then
   $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
 fi
 if [ "$SPARK_ROLE" = "slave" ]; then
-  $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER:$SPARK_MASTER_PORT
+  $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARK_MASTER:$SPARK_MASTER_PORT -p $SPARK_WORKER_PORT
 fi
