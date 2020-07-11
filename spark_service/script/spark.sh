c815effbf24b82380051f2eefde4bf19165358ea
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-07-11 12:24:40 -0300

    trying set memory usage in spark

--- spark_service/script/spark.sh
+++ spark_service/script/spark.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i '1ispark.driver.memory 1g' $SPARK_HOME/conf/spark-defaults.conf
+
 if [ "$SPARK_ROLE" = "master" ]; then
   $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
 fi
