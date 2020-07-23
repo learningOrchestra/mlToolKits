3d36b308f501e3ff9bece1b20791b7986119fa47
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-07-23 05:51:40 -0300

    removing frontend microservice

--- frontend_service/src/assets/icons/Add.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({color='#8D8D8D', size='24'}) => {
-  return (
-    <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill={color}/>
-    </svg>
-  );
-}
