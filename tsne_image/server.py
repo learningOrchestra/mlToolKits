b37b15d2f65528e3520e840ad070f8349cadd7c6
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-08-20 09:18:18 -0300

    creating tsne microservice

---
+++ tsne_image/server.py
@@ -0,0 +1,111 @@
+from flask import jsonify, request, Flask
+import os
+from tsne import (
+    TsneGenerator,
+    MongoOperations,
+    TsneRequestValidator)
+
+HTTP_STATUS_CODE_SUCESS = 200
+HTTP_STATUS_CODE_SUCESS_CREATED = 201
+HTTP_STATUS_CODE_CONFLICT = 409
+HTTP_STATUS_CODE_NOT_ACCEPTABLE = 406
+HTTP_STATUS_CODE_NOT_FOUND = 404
+
+TSNE_HOST_IP = "TSNE_HOST_IP"
+TSNE_HOST_PORT = "TSNE_HOST_PORT"
+
+DATABASE_URL = "DATABASE_URL"
+DATABASE_PORT = "DATABASE_PORT"
+DATABASE_NAME = "DATABASE_NAME"
+DATABASE_REPLICA_SET = "DATABASE_REPLICA_SET"
+
+DOCUMENT_ID = '_id'
+METADATA_DOCUMENT_ID = 0
+
+GET = 'GET'
+POST = 'POST'
+DELETE = 'DELETE'
+
+MESSAGE_RESULT = "result"
+TSNE_FILENAME_NAME = "tsne_filename"
+FIELDS_NAME = "fields"
+
+MESSAGE_CREATED_FILE = "created_file"
+MESSAGE_DELETED_FILE = "deleted_file"
+
+FIRST_ARGUMENT = 0
+
+app = Flask(__name__)
+
+
+def collection_database_url(database_url, database_name, database_filename,
+                            database_replica_set):
+    return database_url + '/' + \
+        database_name + '.' + \
+        database_filename + "?replicaSet=" + \
+        database_replica_set + \
+        "&authSource=admin"
+
+
+@app.route('/tsne/<parent_filename>', methods=[POST])
+def create_tsne(parent_filename):
+    database = MongoOperations(
+        os.environ[DATABASE_URL] + '/?replicaSet=' +
+        os.environ[DATABASE_REPLICA_SET], os.environ[DATABASE_PORT],
+        os.environ[DATABASE_NAME])
+
+    request_validator = TsneRequestValidator(database)
+
+    try:
+        request_validator.tsne_filename_validator(
+            request.json[TSNE_FILENAME_NAME])
+    except Exception as invalid_tsne_filename:
+        return jsonify(
+            {MESSAGE_RESULT:
+                invalid_tsne_filename.args[FIRST_ARGUMENT]}),\
+            HTTP_STATUS_CODE_CONFLICT
+
+    try:
+        request_validator.parent_filename_validator(
+            parent_filename)
+    except Exception as invalid_filename:
+        return jsonify(
+            {MESSAGE_RESULT: invalid_filename.args[FIRST_ARGUMENT]}),\
+            HTTP_STATUS_CODE_NOT_ACCEPTABLE
+
+    database_url_input = collection_database_url(
+                            os.environ[DATABASE_URL],
+                            os.environ[DATABASE_NAME],
+                            parent_filename,
+                            os.environ[DATABASE_REPLICA_SET])
+
+    tsne_generator = TsneGenerator(
+                            database_url_input)
+
+    tsne_generator.create_image(
+        parent_filename,
+        request.json[TSNE_FILENAME_NAME])
+
+    return jsonify(
+        {MESSAGE_RESULT: MESSAGE_CREATED_FILE}),\
+        HTTP_STATUS_CODE_SUCESS_CREATED
+
+
+@app.route('/tsne', methods=[GET])
+def get_images():
+    images = os.listdir('.')
+    return jsonify(
+        {MESSAGE_RESULT: images}),\
+        HTTP_STATUS_CODE_SUCESS
+
+
+@app.route('/tsne/<filename>', methods=[DELETE])
+def delete_image(tsne_filename):
+    os.remove(tsne_filename + ".png")
+    return jsonify(
+        {MESSAGE_RESULT: MESSAGE_DELETED_FILE}),\
+        HTTP_STATUS_CODE_SUCESS
+
+if __name__ == "__main__":
+    app.run(host=os.environ[TSNE_HOST_IP],
+            port=int(os.environ[TSNE_HOST_PORT]))
---
+++ tsne_image/tsne.py
@@ -0,0 +1,107 @@
+from pyspark.sql import SparkSession
+import os
+from pymongo import MongoClient
+import numpy as np
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import OneHotEncoder
+import seaborn as sns
+
+SPARKMASTER_HOST = "SPARKMASTER_HOST"
+SPARKMASTER_PORT = "SPARKMASTER_PORT"
+SPARK_DRIVER_PORT = "SPARK_DRIVER_PORT"
+TSNE_HOST_NAME = "TSNE_HOST_NAME"
+
+
+class DatabaseInterface():
+    def find_one(self, filename, query):
+        pass
+
+    def get_filenames(self):
+        pass
+
+
+class TsneInterface():
+    def create_image(self, filename, tsne_filename):
+        pass
+
+
+class RequestValidatorInterface():
+    MESSAGE_INVALID_FILENAME = "invalid_filename"
+    MESSAGE_DUPLICATE_FILE = "duplicate_file"
+
+    def parent_filename_validator(self, filename):
+        pass
+
+    def tsne_filename_validator(self, tsne_filename):
+        pass
+
+
+class TsneGenerator(TsneInterface):
+    MONGO_SPARK_SOURCE = "com.mongodb.spark.sql.DefaultSource"
+    DOCUMENT_ID = "_id"
+    METADATA_FILE_ID = 0
+
+    def __init__(self, database_url_input):
+        self.spark_session = SparkSession \
+            .builder \
+            .appName("tsne") \
+            .config("spark.mongodb.input.uri",
+                    database_url_input) \
+            .config("spark.driver.port",
+                    os.environ[SPARK_DRIVER_PORT]) \
+            .config("spark.driver.host",
+                    os.environ[TSNE_HOST_NAME])\
+            .config('spark.jars.packages',
+                    'org.mongodb.spark:mongo-spark' +
+                    '-connector_2.11:2.4.2')\
+            .master("spark://" +
+                    os.environ[SPARKMASTER_HOST] +
+                    ':' + str(os.environ[SPARKMASTER_PORT])) \
+            .getOrCreate()
+
+    def create_image(self, filename, tsne_filename):
+        dataframe = self.spark_session.read.format(
+                self.MONGO_SPARK_SOURCE).load()
+        dataframe = dataframe.filter(
+            dataframe[self.DOCUMENT_ID] != self.METADATA_FILE_ID)
+
+        pandas_dataframe = dataframe.toPandas()
+        data_array = OneHotEncoder().fit_transform(pandas_dataframe).toarray()
+
+        treated_array = np.array(data_array)
+        embedded_array = TSNE().fit_transform(treated_array)
+
+        sns_plot = sns.pairplot(embedded_array, size=2.5)
+        sns_plot.savefig(tsne_filename + ".png")
+
+
+class MongoOperations(DatabaseInterface):
+
+    def __init__(self, database_url, database_port, database_name):
+        self.mongo_client = MongoClient(
+            database_url, int(database_port))
+        self.database = self.mongo_client[database_name]
+
+    def find_one(self, filename, query):
+        file_collection = self.database[filename]
+        return file_collection.find_one(query)
+
+    def get_filenames(self):
+        return self.database.list_collection_names()
+
+
+class TsneRequestValidator(RequestValidatorInterface):
+    def __init__(self, database_connector):
+        self.database = database_connector
+
+    def parent_filename_validator(self, filename):
+        filenames = self.database.get_filenames()
+
+        if(filename not in filenames):
+            raise Exception(self.MESSAGE_INVALID_FILENAME)
+
+    def tsne_filename_validator(self, tsne_filename):
+        images = os.listdir('.')
+        if (tsne_filename + ".png") in images:
+            raise Exception(self.MESSAGE_DUPLICATE_FILE)
+
