9c2af8aecbd1e1b76cdc0e578edd00c768d569ac
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-09-16 15:32:51 -0300

    bug fix

--- microservices/tsne_image/tsne.py
+++ microservices/tsne_image/tsne.py
@@ -95,11 +95,11 @@ class TsneGenerator(TsneInterface):
             embedded_array[label_name] = encoded_dataframe[label_name]
             sns_plot = sns.scatterplot(
                 data=embedded_array, size=self.IMAGE_SIZE, hue=label_name)
-            sns_plot.savefig(image_path)
+            sns_plot.get_figure().savefig(image_path)
         else:
             sns_plot = sns.scatterplot(
                 data=embedded_array, size=self.IMAGE_SIZE)
-            sns_plot.savefig(image_path)
+            sns_plot.get_figure().savefig(image_path)
 
     def file_processor(self):
         file = self.spark_session.read.format(
