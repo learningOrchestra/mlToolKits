c96c81694cd9ce442bd7bb0e8ce0ddf403c4f5b6
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-09-17 13:13:29 -0300

    bug fix

--- microservices/pca_image/pca.py
+++ microservices/pca_image/pca.py
@@ -98,7 +98,7 @@ class PcaGenerator(PcaInterface):
                 data=embedded_array, size=self.IMAGE_SIZE, hue=label_name)
             sns_plot.get_figure().savefig(image_path)
         else:
-            sns_plot = sns.scatterplot(x="0", y="1",
+            sns_plot = sns.scatterplot(x=0, y=1,
                 data=embedded_array, size=self.IMAGE_SIZE)
             sns_plot.get_figure().savefig(image_path)
 
