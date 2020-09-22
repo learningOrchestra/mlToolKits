c2b60c50ff7bdc90854c9d356d14a2373c8e4c89
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-09-22 19:39:53 -0300

    update seaborn version

--- microservices/tsne_image/tsne.py
+++ microservices/tsne_image/tsne.py
@@ -99,7 +99,7 @@ class TsneGenerator(TsneInterface):
             sns_plot = sns.scatterplot(
                 x=0, y=1,
                 data=embedded_array, size=self.IMAGE_SIZE,
-                hue=[label_name])
+                hue=label_name)
             sns_plot.get_figure().savefig(image_path)
         else:
             print("not label", flush=True)
