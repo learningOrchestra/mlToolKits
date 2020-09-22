d7f030dbd9ee1c2af5d06c27912695691ce4a210
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-09-22 15:48:00 -0300

    setting legend as False

--- microservices/tsne_image/tsne.py
+++ microservices/tsne_image/tsne.py
@@ -98,14 +98,14 @@ class TsneGenerator(TsneInterface):
 
             sns_plot = sns.scatterplot(x=0, y=1,
                 data=embedded_array, size=self.IMAGE_SIZE,
-                hue=label_name, legend="brief")
+                hue=label_name, legend=False)
             sns_plot.get_figure().savefig(image_path)
         else:
             print("not label", flush=True)
 
             sns_plot = sns.scatterplot(x=0, y=1,
                 data=embedded_array, size=self.IMAGE_SIZE,
-                hue=None, legend="full")
+                hue=None, legend=False)
             sns_plot.get_figure().savefig(image_path)
 
     def file_processor(self):
