1d5f82eb1eaf46da3bfa038e11a50b955fcc77f8
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-09-22 19:18:27 -0300

    remove legend parameter

--- microservices/tsne_image/tsne.py
+++ microservices/tsne_image/tsne.py
@@ -98,14 +98,14 @@ class TsneGenerator(TsneInterface):
 
             sns_plot = sns.scatterplot(x=0, y=1,
                 data=embedded_array, size=self.IMAGE_SIZE,
-                hue=label_name, legend=False)
+                hue=label_name)
             sns_plot.get_figure().savefig(image_path)
         else:
             print("not label", flush=True)
 
             sns_plot = sns.scatterplot(x=0, y=1,
                 data=embedded_array, size=self.IMAGE_SIZE,
-                hue=None, legend=False)
+                hue=None)
             sns_plot.get_figure().savefig(image_path)
 
     def file_processor(self):
