a0fa484e38d01367078ffe83d71ba72afb46e8a4
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2020-09-29 06:25:23 -0300

    update python package version

--- learning_orchestra_client/setup.py
+++ learning_orchestra_client/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="learning_orchestra_client",
-    version="0.6.0",
+    version="1.0.0",
     author="Gabriel Ribeiro",
     author_email="gabbriel.rribeiro@gmail.com",
     description="Learning Orchestra client for Python",
