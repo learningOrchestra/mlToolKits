bd702b92535736594eac19c90a14786aafedba9b
riibeirogabriel <gabbriel.rribeiro@gmail.com>
2021-04-27 14:31:27 -0300

    bug fix

--- microservices/code_executor_image/utils.py
+++ microservices/code_executor_image/utils.py
@@ -174,14 +174,19 @@ class ObjectStorage:
     def read(self, filename: str, service_type: str) -> object:
         binary_path = ObjectStorage.get_read_binary_path(
             filename, service_type)
+        binary_instance = open(
+            binary_path,
+            self.__READ_OBJECT_OPTION)
+
         try:
-            model_binary_instance = open(
-                binary_path,
-                self.__READ_OBJECT_OPTION)
-            return dill.load(model_binary_instance)
+            return dill.load(binary_instance)
         except Exception:
             traceback.print_exc()
-            return keras.models.load_model(binary_path)
+            try:
+                return keras.models.load_model(binary_path)
+            except Exception:
+                traceback.print_exc()
+                return binary_instance
 
     def save(self, instance: object, filename: str) -> None:
         output_path = ObjectStorage.get_write_binary_path(filename)
