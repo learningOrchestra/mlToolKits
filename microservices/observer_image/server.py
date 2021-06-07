cf8b9952a78ffdfeb5a09e1b2cf29e23bcf073dd
SGMPAndre <SGMP_Andre>
2021-06-07 06:27:15 -0300

    added / character to POST return

--- microservices/observer_image/server.py
+++ microservices/observer_image/server.py
@@ -78,7 +78,7 @@ def create_collection_watcher(filename:str) -> jsonify:
                                pipeline=pipeline,
                                timeout=timeout)
 
-        return successful_response(Constants.MICROSERVICE_URI_PATH +
+        return successful_response(Constants.MICROSERVICE_URI_PATH + '/' +
                                    cursor_name)
     except:
         return error_response(Constants.MESSAGE_RESPONSE_FILENAME +
