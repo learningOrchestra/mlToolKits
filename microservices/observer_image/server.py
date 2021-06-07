f1fc6c69288cc63f260f2a5e997679e7f2ddca0e
SGMPAndre <SGMP_Andre>
2021-06-07 06:19:14 -0300

    fixed wrong uri return in POST method

--- microservices/observer_image/server.py
+++ microservices/observer_image/server.py
@@ -78,7 +78,8 @@ def create_collection_watcher(filename:str) -> jsonify:
                                pipeline=pipeline,
                                timeout=timeout)
 
-        return successful_response(Constants.API_PATH + cursor_name)
+        return successful_response(Constants.MICROSERVICE_URI_PATH +
+                                   cursor_name)
     except:
         return error_response(Constants.MESSAGE_RESPONSE_FILENAME +
                               filename)
