8129e0313143448154b1a71ec51ef41b9e4a8bc1
SGMPAndre <SGMP_Andre>
2021-06-18 07:25:01 -0300

    bugfix

--- microservices/observer_image/utils/database.py
+++ microservices/observer_image/utils/database.py
@@ -50,7 +50,7 @@ class Database:
         print('p1.3', flush=True)
         if observer_name == '':
             observer_name = self.__get_default_observer_name(collection_name)
-        elif collection_name in self.cursors_array.keys()
+        elif collection_name in self.cursors_array.keys():
             if observer_name in self.cursors_array[collection_name].keys():
                 return f'{collection_name}/{observer_name}'
 
