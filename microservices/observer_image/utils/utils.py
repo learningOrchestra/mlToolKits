322e7ec01a6de7e8caa945ba360a138ac432586d
SGMPAndre <SGMP_Andre>
2021-06-20 06:16:40 -0300

    bug fix

--- microservices/observer_image/utils/utils.py
+++ microservices/observer_image/utils/utils.py
@@ -13,31 +13,21 @@ class Utils:
     def call_cursor_with_timeout(self,cursor: CollectionChangeStream,
                                  timeout: int = 0):
 
-        print(f"waiting2!! {timeout}", flush=True)
         process = Process(target=self.__call_mongo_watcher,
                           name='call_mongo_watcher')
 
-        print(f"waiting3!! {timeout}", flush=True)
         self.__CURSOR = cursor
         process.start()
 
-        print(f"waiting timeout = {timeout}",flush=True)
         if timeout <= 0:
             process.join()
         else:
             process.join(timeout=timeout)
 
-        print(f"finished timeout = {timeout}",flush=True)
-
-        print(f"error?",flush=True)
-
         if process.exitcode is None:
-            print(f"error2?", flush=True)
-            process.terminate()
             return None
 
         process.terminate()
-        print(f"timeout?",flush=True)
 
         result = self.__MONGO_NEXT_RESULT
         self.__MONGO_NEXT_RESULT = None
